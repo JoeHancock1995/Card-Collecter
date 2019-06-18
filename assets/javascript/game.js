d1c1b2f5b4f7704d98b75bfd2b7e00fdbd391afd
Joe Hancock <joe.hancock95@gmail.com>
2019-06-18 14:40:59 -0500

    beginning of js and jquery code

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    var wins = 0;
+    var losses = 0;
+    // this is the random number generated//
+    var scoreGiven = 0;
+    var userScore = 0;
+
+    $("#cards").click(function(){
+        
+    })
+});
+
