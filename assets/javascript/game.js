030ba8431ebe936896ffe7a615912de623c0a011
Joe Hancock <joe.hancock95@gmail.com>
2019-06-18 23:18:13 -0500

    js var creations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,23 @@ $(document).ready(function() {
 
     var wins = 0;
     var losses = 0;
-    // this is the random number generated//
-    var scoreGiven = 0;
     var userScore = 0;
+    function getCardNumber () {
+        return (Math.floor(Math.random() * 12 + 1));
+    }
 
-    $("#cards").click(function(){
-        
-    })
+    var minimum = 19;
+    var maximum = 120;
+    var randomNumber;
+    function getRandomNumber () {
+        return (Math.floor(Math.random() * (maximum - minimum + 1) ) + minimum);
+        }
+
+var cardArray = [];
+var emperor;
+var priestess;
+var magician;
+var strength;
+
 });
 
