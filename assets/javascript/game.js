ec53e78988f7b87bba18247b4c907ac22db4dc3b
Joe Hancock <joe.hancock95@gmail.com>
2019-06-20 01:45:57 -0500

    emperor button done, rand number done, and user score done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,22 @@
-$(document).ready(function() {
+var wins = 0;
+var losses = 0;
+var userScore = 0;
+function getCardNumber () {
+    return (Math.floor(Math.random() * 12 + 1));
+}
 
-    var wins = 0;
-    var losses = 0;
-    var userScore = 0;
-    function getCardNumber () {
-        return (Math.floor(Math.random() * 12 + 1));
-    }
-
-    var minimum = 19;
-    var maximum = 120;
-    var randomNumber;
-    function getRandomNumber () {
-        return (Math.floor(Math.random() * (maximum - minimum + 1) ) + minimum);
-        }
+var minimum = 19;
+var maximum = 120;
+var randomNumber;
+function getRandomNumber () {
+    return (Math.floor(Math.random() * (maximum - minimum + 1) ) + minimum);
+}
 
 var cardArray = [];
-var emperor;
-var priestess;
-var magician;
-var strength;
-
-$(document).ready(function(){
-    $(":button");
-  });
+var emperorCard;
+var priestessCard;
+var magicianCard;
+var strengthCard;
 
 function setWins() {
     $("#wins").append(wins);
@@ -40,16 +34,90 @@ function setRandomNumber () {
 function setTotalScore() {
     userScore = 0;
     $("#totalScore").append(userScore);
-
-    function setCardNumber() {
-        emperor = getCardNumber();
-        priestess = getCardNumber();
-        magician = getCardNumber();
-        strength = getCardNumber();
-    
-    }
 }
 
+function setCardNumber() {
+    emperorCard = getCardNumber();
+    priestessCard = getCardNumber();
+    magicianCard = getCardNumber();
+    strengthCard = getCardNumber();
+    $("#emperorText").append(emperorCard);
+    $("#priestessText").append(priestessCard);
+    $("#magicianText").append(magicianCard);
+    $("#strengthText").append(strengthCard);
+    cardArray.push(emperorCard, priestessCard, magicianCard, strengthCard);
+    console.log(cardArray);
 
+        for (var iterator = 0; iterator > cardArray.length; iterator++) {
+            if (cardArray.indexOf(iterator)) {
+                console.log(true);
+                $("#emperorText, #priestessText, magicianText, #strengthText").empty();
+                setCardNumber();
+            }
+            else {
+                console.log(false);
+            }
+        }
+    }
+
+
+function hideNumbers() {
+    $("#emperorText").css("display", "none");
+    $("#priestessText").css("display", "none");
+    $("#magicianText").css("display", "none");
+    $("#strengthText").css("display", "none");
+}
+
+function showEmperor () {
+    $("#emperorText").css("display", "block");
+}
+
+function showPriestess () {
+    $("#priestessText").css("display", "block");
+}
+
+function showMagician () {
+    $("#magicianText").css("display", "block");
+}
+
+function showStrength () {
+    $("#estrengthText").css("display", "block");
+}
+
+function reset() {
+    $("#totalScore").empty();
+    $("#randomNumber").empty();
+    $("#emperorText, #priestessText, #magicianText, strengthText").empty();
+    cardArray.empty();
+    setRandomNumber();
+    setTotalScore();
+    setCardNumber();
+    hideNumbers();
+}
+
+$(".button").one("click", function startGame () {
+    setWins();
+    setLosses();
+    setRandomNumber();
+    setTotalScore();
+    setCardNumber();
+    hideNumbers();
 });
 
+$("#emperorCard").click(function () {
+    showEmperor();
+    userScore = userScore + emperorCard;
+    console.log("Current score: + userScore");
+    $('#totalScore').text(userScore);
+        if (userScore === randomNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            alert("You win!");
+            reset();
+        } else if (userScore > randomNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            alert("You lose");
+            reset();
+        }
+});
